bcbd10414fb94d5d65c7f6382ce3955598d53406
tungmtp <75711916+tungmtp@users.noreply.github.com>
2022-04-21 21:11:50 +0700

    Add files via upload

---
+++ server.cpp
@@ -0,0 +1,53 @@
+﻿#include <stdio.h>
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32")
+#pragma warning(disable:4996)
+
+int main()
+{
+    // Khoi tao thu vien
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    // Tao socket
+    SOCKET listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    // Khai bao dia chi server
+    SOCKADDR_IN addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(8000);
+
+    int ret;
+    // Gan cau truc dia chi voi socket
+    bind(listener, (SOCKADDR*)&addr, sizeof(addr));
+
+    // Chuyen sang trang thai cho ket noi
+    listen(listener, 5);
+
+    // Cho ket noi moi
+    SOCKADDR_IN clientAddr;
+    int clientAddrLen = sizeof(clientAddr);
+
+    SOCKET client = accept(listener, (SOCKADDR*)&clientAddr, &clientAddrLen);
+   
+
+
+    char buf[256];
+    while (1)
+    {
+        ret = recv(client, buf, sizeof(buf), 0);
+        if (ret <= 0)
+            break;
+
+        // Them ky tu ket thuc xau va in ra man hinh
+        buf[ret] = 0;
+        printf("%s", buf);
+    }
+    printf("\nket noi: %d, dia chi IP: %s", client, inet_ntoa(clientAddr.sin_addr));
+    // Dong ket noi va giai phong thu vien
+    closesocket(client);
+    closesocket(listener);
+    WSACleanup();
+}
